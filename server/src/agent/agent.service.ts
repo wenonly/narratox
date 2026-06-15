0309b6dea26d2aa2a2b8e9df209ed15d8d429353
wenonly <1325772089@qq.com>
2026-06-15 23:04:13 +0800

    feat: update

---
+++ server/src/agent/agent.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { CreateAgentDto } from './dto/create-agent.dto';
+import { UpdateAgentDto } from './dto/update-agent.dto';
+
+@Injectable()
+export class AgentService {
+  create(createAgentDto: CreateAgentDto) {
+    return 'This action adds a new agent';
+  }
+
+  findAll() {
+    return `This action returns all agent`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} agent`;
+  }
+
+  update(id: number, updateAgentDto: UpdateAgentDto) {
+    return `This action updates a #${id} agent`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} agent`;
+  }
+}
